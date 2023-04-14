1cf682e1e2c59d8a259fbf603d66acc35cce27a0
athulAnilkumarAppu <athulAnilkumarAppu@gmail.com>
2023-04-14 13:27:58 +0530

    firebase changed

--- src/services/downloadPdfService.js
+++ src/services/downloadPdfService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const getPdfService = async ()=> {
-    const res = await axios.get('/fetch-pdf', { responseType: 'blob' }).then((response)=> {
+    const res = await axios.get('https://server-backend-resume.onrender.com/fetch-pdf', { responseType: 'blob' }).then((response)=> {
         const pdfBlob = new Blob([response.data], { type: 'application/pdf' })  // return the pdf file
         return pdfBlob
     }).catch((error)=> {
--- src/services/sendResumeDataService.js
+++ src/services/sendResumeDataService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const sendResumeDataService = async (params)=> {
-   const res =  await axios.post('/create-pdf', params).then((response)=> {
+   const res =  await axios.post('https://server-backend-resume.onrender.com/create-pdf', params).then((response)=> {
         return response
     }).catch((error)=> {
         console.log(error)
